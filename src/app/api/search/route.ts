36976de3eafd74bc0a05af28c43997a2b9706617
mint <ge10rik@gmail.com>
2023-11-23 12:37:29 +0700

    fixed determining query param

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -23,9 +23,10 @@ const ipifyAPI = (query: string) => {
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const query =
-    searchParams.get('query') || isIP(request.headers.get('x-forwarded-for') ?? '')
+    searchParams.get('query') ||
+    (isIP(request.headers.get('x-forwarded-for') ?? '')
       ? request.headers.get('x-forwarded-for') ?? ''
-      : '';
+      : '');
 
   let res;
 
